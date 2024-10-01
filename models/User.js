dfb00431e8315d989d762e5d543582a1f5fe850d
Aniket Solanki <aniketsolankianiket48@gmail.com>
2024-10-02 02:04:30 +0530

    fix user signup

--- models/User.js
+++ models/User.js
@@ -21,7 +21,7 @@ const UserSchema = new Schema({
 // Use a partial index to ignore documents where bookmarks.story or bookmarks.index is null
 UserSchema.index(
     { 'bookmarks.story': 1, 'bookmarks.index': 1 },
-    { unique: true, partialFilterExpression: { 'bookmarks.story': { $exists: true }, 'bookmarks.index': { $exists: true } } }
+    { unique: true, partialFilterExpression: { 'bookmarks.story': { $ne: null }, 'bookmarks.index': { $ne: null } } }
 );
 
 module.exports = mongoose.model('User', UserSchema);
