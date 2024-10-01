2f745cdff2f8dbeb0bdca277ba358a3f7a9af19a
Aniket Solanki <aniketsolankianiket48@gmail.com>
2024-10-01 22:23:54 +0530

    you

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // Load environment variables
 dotenv.config();
